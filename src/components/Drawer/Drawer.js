ca23f3b7392cc0312289ecb50c0640b9f45bcd07
Kaleween <vanmael.kevin@gmail.com>
2020-11-13 12:06:48 +0100

    Change font size

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -34,6 +34,7 @@ const useStyles = makeStyles((theme) => ({
         // necessary for content to be below app bar
         ...theme.mixins.toolbar,
         justifyContent: 'flex-end',
+        fontSize: '20px',
     },
     drawerFooter: {
         position: 'absolute',
